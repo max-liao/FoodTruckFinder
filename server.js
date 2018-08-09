29050fd5eb51e9d2fc3aeca42b2a6c2a957143cf
Max Liao <max.liao@gmail.com>
2018-08-09 15:04:53 -0400

    package.json

--- server.js
+++ server.js
@@ -18,30 +18,6 @@ app.use(bodyParser.json());
 var routes = require('./app/controllers/controller.js');
 app.use(routes);
 
-
-app.use(function (req, res, next) {
-  var _res$locals = res.locals,
-      component = _res$locals.component,
-      initialState = _res$locals.initialState;
-
-  if (!component) {
-    return next();
-  }
-
-  res.format({
-    'text/html': function textHtml() {
-      var appString = (0, _server.renderToString)(_react2.default.createElement(component, initialState));
-      res.send((0, _template2.default)({
-        body: appString,
-        title: 'Food Truck Finder',
-        initialState: JSON.stringify(initialState)
-      }));
-    },
-  });
-
-  next();
-});
-
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
