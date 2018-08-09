386daf839fd2fc3c6a5aa6fc58a529e99c52c3d6
Max Liao <max.liao@gmail.com>
2018-08-09 15:07:31 -0400

    package.json

--- app/public/js/twitter.js
+++ app/public/js/twitter.js
@@ -1,7 +1,6 @@
 var Twitter = require('twitter');
 require("dotenv").config();
 
-
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -9,11 +8,7 @@ var client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-
-
 var twitter = {
-
-
     getTweets: function(handle, cb) {
         client.get('statuses/user_timeline',{ screen_name: handle, limit: '1'} , function(error, tweets, response) {
         if (!error){
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-
 var PORT = process.env.PORT || 8080;
 
 var app = express();
