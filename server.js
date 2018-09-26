d40a6769fe4aecdc29bbe8e78d93cf7a1256f7e3
Max Liao <max.liao@gmail.com>
2018-09-26 14:04:31 -0400

    gcloud

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ require('dotenv').config();
 
 var PORT = process.env.PORT || 8080;
 
+exports.envVar = (req, res) => {
+  res.send(process.env.GoogleCloud);
+};
+
 console.log(process.env.GoogleCloud)
 
 var app = express();
